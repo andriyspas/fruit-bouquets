a6afd7a785f10d93790065804e9637ebef5d4703
andriyspas <spas-andriy2008@yandex.ru>
2017-08-01 22:21:05 +0200

    added example scroll section component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 import { Header } from './components/index';
 
+import Scroll from 'react-scroll';
+let Element = Scroll.Element;
+
 class App extends Component {
     render() {
         return (
             <div>
                 <Header/>
+
+                <Element name="test1" className="element">
+                    test 1
+                </Element>
+
+                <Element name="test2" className="element">
+                    test 2
+                </Element>
+
+                <Element name="test3" className="element">
+                    test 3 (anchor)
+                </Element>
             </div>
         )
     }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,39 @@
 import React, { Component } from 'react';
+import Scroll from 'react-scroll';
 
-const Header = () =>
-    <header>Hello world</header>;
+let Link = Scroll.Link;
+
+let scroll = Scroll.animateScroll;
+
+class Header extends Component {
+
+    constructor() {
+        super();
+    }
+
+    scrollToTop = () => {
+        scroll.scrollToTop();
+    };
+
+    render() {
+        return (
+            <div style={{ position : 'fixed' }}>
+                <Link activeClass="active" to="test1" spy={true} smooth={true} offset={50} duration={500} onSetActive={this.handleSetActive}>
+                    Test 1
+                </Link>
+
+                <Link activeClass="active" to="test2" spy={true} smooth={true} offset={50} duration={500}>
+                    Test 2 (delay)
+                </Link>
+
+                <Link activeClass="active" to="test3" spy={true} smooth={true} offset={50} duration={500}>
+                    Test 3 (anchor)
+                </Link>
+
+                <a onClick={ this.scrollToTop }>To the top!</a>
+            </div>
+        );
+    }
+}
 
 export default Header;
