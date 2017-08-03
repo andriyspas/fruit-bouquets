411091edd643b4db68d97ce9d70c3955fde5e503
staspast <staspast1@gmail.com>
2017-08-03 17:54:56 +0200

    Added language internationalization

--- src/App.js
+++ src/App.js
@@ -1,32 +1,72 @@
-import React, { Component } from 'react';
-import { Header, Home, Contact, Navigation, Reviews } from './components/index';
+import React, { Component } from 'react'
+import { Header, Home, Contact, Navigation, Reviews } from './components/index'
 
-import Scroll from 'react-scroll';
-let Element    = Scroll.Element;
+import Scroll from 'react-scroll'
+import { IntlProvider } from 'react-intl';
+import {addLocaleData} from 'react-intl';
+import uk from 'react-intl/locale-data/uk';
+import pl from 'react-intl/locale-data/pl';
+
+addLocaleData(uk)
+addLocaleData(pl)
+
+let messagesUa = require('./locale/locale_ua.json');
+let messagesEn = require('./locale/locale_en.json');
+let messagesPl = require('./locale/locale_pl.json');
+let localeUa = 'uk';
+let localeEn = 'en';
+let localePl = 'pl';
+
+let Element = Scroll.Element
 
 class App extends Component {
-    render() {
-        return (
-            <div>
-                <Header/>
 
-                <Navigation/>
-
-                <Element name="home">
-                    <Home/>
-                </Element>
-
-                <Element name="reviews">
-                    <Reviews/>
-                </Element>
-
-                <Element name="contact">
-                    <Contact/>
-                </Element>
-
-            </div>
-        )
+  constructor (){
+    super()
+    this.state = {
+      locale: localeUa,
+      messages: messagesUa
     }
+  }
+
+  handleLanguageChange = (lan) =>{
+    switch (lan){
+      case 'ua':
+        this.setState({messages: messagesUa, locale: localeUa})
+        break
+      case 'en':
+        this.setState({messages: messagesEn, locale: localeEn})
+        break
+      case 'pl':
+        this.setState({messages: messagesPl, locale: localePl})
+        break
+    }
+  }
+
+  render () {
+    return (
+      <IntlProvider locale={this.state.locale} messages={this.state.messages}>
+        <div>
+          <Header onLanguageChange={this.handleLanguageChange}/>
+
+          <Navigation/>
+
+          <Element name="home">
+            <Home/>
+          </Element>
+
+          <Element name="reviews">
+            <Reviews/>
+          </Element>
+
+          <Element name="contact">
+            <Contact/>
+          </Element>
+
+        </div>
+      </IntlProvider>
+    )
+  }
 }
 
-export default App;
+export default App
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,32 @@
 import React, { Component } from 'react'
 import { Navbar, NavItem, NavDropdown, MenuItem, Nav, OverlayTrigger } from  'react-bootstrap';
 import PopoverPhone from './Popover/Popover';
+import {FormattedMessage} from 'react-intl'
+
 
 class Header extends Component {
+
+  handleSelect = (eventKey) => {
+    this.props.onLanguageChange(eventKey)
+  }
 
     render() {
         return (
             <Navbar fluid>
                 <Navbar.Header>
                     <Navbar.Brand>
-                        <a href="/">Title</a>
+                        <a href="/"><FormattedMessage id="title"/></a>
                     </Navbar.Brand>
 
                     <Navbar.Toggle />
                 </Navbar.Header>
 
                 <Navbar.Collapse>
-                    <Nav pullRight>
-                        <NavDropdown eventKey={1} title="UKR" id="dropdown">
-                            <MenuItem>ENG</MenuItem>
-                            <MenuItem>POL</MenuItem>
-                            <MenuItem>RUS</MenuItem>
+                    <Nav pullRight onSelect={this.handleSelect}>
+                        <NavDropdown eventKey={1} title="Lang" id="dropdown">
+                            <MenuItem eventKey='en'>ENG</MenuItem>
+                            <MenuItem eventKey='pl'>POL</MenuItem>
+                            <MenuItem eventKey='ua'>UKR</MenuItem>
                         </NavDropdown>
 
                         <OverlayTrigger
