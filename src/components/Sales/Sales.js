1b93c94be399220ede1f318846d6228bf30b63bf
andriyspas <spas-andriy2008@yandex.ru>
2017-08-09 14:12:16 +0200

    rwd version sales and reviews pages

--- src/components/Sales/Sales.js
+++ src/components/Sales/Sales.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Parallax, Background} from 'react-parallax';
+import {Grid} from 'react-bootstrap'
 
 class Sales extends Component {
     render() {
@@ -11,42 +12,52 @@ class Sales extends Component {
                     </Background>
 
                     <div className="sales__content">
+                        <Grid>
+                            <div className="sales__single--wrapper">
+                                <div className="sales__single">
+                                    <div className="sales__round--wrapper">
+                                        <div className="sales__round">
+                                            <img src={require('../../images/money-bag.svg')} alt=""/>
+                                        </div>
+                                    </div>
 
-                        <div className="sales__single">
-                            <div className="sales__round--wrapper">
-                                <div className="sales__round">
-                                    <img src={require('../../images/money-bag.svg')} alt=""/>
+                                    <div className="sales__description">
+                                        <div className="sales__name">Lorem ipsum</div>
+                                        <div className="sales__details">Lorem ipsum doloresm test big name</div>
+                                    </div>
                                 </div>
                             </div>
-                            <div>
-                                <div className="sales__name">Lorem ipsum</div>
-                                <div className="sales__details">Lorem ipsum doloresm test big name</div>
-                            </div>
-                        </div>
 
-                        <div className="sales__single">
-                            <div className="sales__round--wrapper">
-                                <div className="sales__round">
-                                    <img src={require('../../images/piggy-bank.svg')} alt=""/>
+                            <div className="sales__single--wrapper">
+                                <div className="sales__single">
+                                    <div className="sales__round--wrapper">
+                                        <div className="sales__round">
+                                            <img src={require('../../images/piggy-bank.svg')} alt=""/>
+                                        </div>
+                                    </div>
+
+                                    <div className="sales__description">
+                                        <div className="sales__name">Lorem ipsum</div>
+                                        <div className="sales__details">Lorem ipsum doloresm test big name</div>
+                                    </div>
                                 </div>
                             </div>
-                            <div>
-                                <div className="sales__name">Lorem ipsum</div>
-                                <div className="sales__details">Lorem ipsum doloresm test big name</div>
-                            </div>
-                        </div>
 
-                        <div className="sales__single">
-                            <div className="sales__round--wrapper">
-                                <div className="sales__round">
-                                    <img src={require('../../images/price-tag.svg')} alt=""/>
+                            <div className="sales__single--wrapper">
+                                <div className="sales__single">
+                                    <div className="sales__round--wrapper">
+                                        <div className="sales__round">
+                                            <img src={require('../../images/price-tag.svg')} alt=""/>
+                                        </div>
+                                    </div>
+
+                                    <div className="sales__description">
+                                        <div className="sales__name">Lorem ipsum</div>
+                                        <div className="sales__details">Lorem ipsum doloresm test big name</div>
+                                    </div>
                                 </div>
                             </div>
-                            <div>
-                                <div className="sales__name">Lorem ipsum</div>
-                                <div className="sales__details">Lorem ipsum doloresm test big name</div>
-                            </div>
-                        </div>
+                        </Grid>
                     </div>
                 </Parallax>
             </div>
